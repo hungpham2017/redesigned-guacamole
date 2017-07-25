c244b89296efd0dc898761dcecc72cb746425f9d
Hung Pham <phamx494@login01.msi.umn.edu>
2017-07-25 13:29:57 -0500

    .

---
+++ redesigned_guacamole/__init__.py
@@ -0,0 +1,8 @@
+'''
+This is the init file of the redesigned-guacamole
+'''
+
+from . import math
+from .math import mult
+from .math import dev
+from .math import power
---
+++ redesigned_guacamole/math.py
@@ -0,0 +1,22 @@
+'''
+A small set of functitons for doing math operation
+'''
+
+def add(a, b):
+	'''
+		Add two values together
+	'''
+	return a + b
+	
+def mult(a, b):
+	'''
+		Multiply two values together
+	'''
+	return a * b
+	
+def dev(a, b ):
+	'''
+		Multiply two values together
+	'''
+	return a / b	
+		
